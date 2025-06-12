ce9fa4f231831a54e37135f72de8533a55cea48e
GonzalesAndy <andygonzales83@gmail.com>
2025-06-13 00:54:51 +0200

    yay new project

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(() => console.log('✅ Service worker enregistré.'))
+      .catch(err => console.error('❌ Erreur service worker :', err));
+  }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".game-card");
+
+  cards.forEach((card, index) => {
+    card.style.animationDelay = `${index * 200}ms`;
+  });
+});
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const URLS_TO_CACHE = [
+  'index.html',
+  'offline.html',
+  'style.css'
+];
+
+// Installation
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+// Activation (nettoyage de l'ancien cache)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+      .then(response => response || caches.match('offline.html'))
+  );
+});
+
+
